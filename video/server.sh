1f42269987640acc78eec4a761b272e5f3e83dc5
Vyacheslav Koscheev <vok1980+rpi@gmail.com>
2021-02-19 00:15:35 +0700

    add raspivid options

--- video/server.sh
+++ video/server.sh
@@ -1,1 +1,1 @@
-raspivid -t 999999 -b 2000000 -o - | gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=192.168.0.110 port=5001
+raspivid -n -w 920 -h 540 -t 999999 -b 2000000 -o - | gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=192.168.0.110 port=5001
