a2791d6ff1fa8d2565691f4a8b29b06efdbe13a2
Vyacheslav Koscheev <vok1980+rpi@gmail.com>
2021-02-19 10:58:19 +0700

    Specify codec

--- video/server.sh
+++ video/server.sh
@@ -4,5 +4,6 @@ raspivid \
     --height 540 \
     --timeout 999999 \
     --bitrate 2000000 \
+    --codec H264 \
     --output - \
 | gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=192.168.0.110 port=5001
